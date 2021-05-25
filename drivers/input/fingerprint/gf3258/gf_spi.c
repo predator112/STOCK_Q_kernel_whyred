65b8ad147f217ad9e38b2a85449affe24b1cfb3b
Oktapra Amtono <oktapra.amtono@gmail.com>
2021-05-25 13:38:56 +0700

    drivers/input: fingerprint: do not create proc_entry
    * this also shut up log spam dmesg
    
    [    0.449660] ------------[ cut here ]------------
    [    0.449662] WARNING: at ../fs/proc/generic.c:345
    [    0.449664]
    [    0.449671] CPU: 4 PID: 1 Comm: swapper/0 Tainted: G        W       4.4.268-Mystic-EAS_beta-x6 #1
    [    0.449674] Hardware name: Qualcomm Technologies, Inc. SDM 636 PM660 + PM660L E7S (DT)
    [    0.449677] task: ffffffed794c0000 task.stack: ffffffed794c8000
    [    0.449680] pc : [<ffffff840100b098>] lr : [<ffffff840100b094>] pstate: 20000005
    [    0.449682] sp : ffffffed794cbc20
    [    0.449683] x29: 0000000000000000 x28: ffffff84023098b8
    [    0.449686] x27: ffffff84020e67d8 x26: 00000000ffffffff
    [    0.449689] x25: ffffffed77c45940 x24: ffffffed78510250
    [    0.449691] x23: 0000000000000006 x22: ffffffed77c45e85
    [    0.449694] x21: ffffff84025bb668 x20: ffffff84025bb6a0
    [    0.449696] x19: ffffffed77c45e00 x18: 00000000000fb518
    [    0.449698] x17: 0000000000000050 x16: 0000000000000030
    [    0.449700] x15: ffffffed7e2b7e48 x14: 0000000000003434
    [    0.449702] x13: 0000000000001190 x12: 0000000000003936
    [    0.449704] x11: 0000000000000000 x10: 0000000000000000
    [    0.449706] x9 : ffffff84025b2390 x8 : 0000000000000001
    [    0.449709] x7 : 6c6120276f666e69 x6 : ffffff84027c9078
    [    0.449711] x5 : 0000000000000000 x4 : 0000000000000008
    [    0.449713] x3 : 6465726574736967 x2 : 0000000000000028
    [    0.449715] x1 : 0000000000000000 x0 : ffffff84027d3000
    [    0.449719] \x0aPC: 0xffffff840100b058:
    [    0.449721] b058  9000be40 91251000 9438fa3c 2a1f03e0 a9444ff4 a94357f6 a9425ff8 a94167fa
    [    0.449731] b078  f84507fe d65f03c0 90008760 910216a1 9109fc00 aa1603e2 97fb974e 9000be40
    [    0.449737] b098  d4210000 91251000 9438fa2c b9400275 9000be53 91250273 aa1303e0 9438f8d9
    [    0.449743] b0b8  aa0003f4 52a20008 9000be40 0b0802a1 91244000 94064aa5 aa1303e0 aa1403e1
    [    0.449749] \x0aLR: 0xffffff840100b054:
    [    0.449751] b054  94065fe7 9000be40 91251000 9438fa3c 2a1f03e0 a9444ff4 a94357f6 a9425ff8
    [    0.449756] b074  a94167fa f84507fe d65f03c0 90008760 910216a1 9109fc00 aa1603e2 97fb974e
    [    0.449762] b094  9000be40 d4210000 91251000 9438fa2c b9400275 9000be53 91250273 aa1303e0
    [    0.449767] b0b4  9438f8d9 aa0003f4 52a20008 9000be40 0b0802a1 91244000 94064aa5 aa1303e0
    [    0.449774] \x0aSP: 0xffffffed794cbbe0:
    [    0.449775] bbe0  0100b094 ffffff84 794cbc20 ffffffed 0100b098 ffffff84 20000005 00000000
    [    0.449781] bc00  794cbb90 ffffffed ffffffc8 ffffff80 ffffffff ffffffff 0100b094 ffffff84
    [    0.449786] bc20  0100b404 ffffff84 020e67d8 ffffff84 00000000 00000000 02331e60 ffffff84
    [    0.449792] bc40  0000005f 00000000 028c4f88 ffffff84 028c4000 ffffff84 77c45e00 ffffffed
    [    0.449798]
    [    0.449801] ---[ end trace b96cd82e0126231e ]---
    
    Signed-off-by: Oktapra Amtono <oktapra.amtono@gmail.com>

--- drivers/input/fingerprint/gf3258/gf_spi.c
+++ drivers/input/fingerprint/gf3258/gf_spi.c
@@ -43,7 +43,6 @@
 #include <linux/cpufreq.h>
 #include <linux/mdss_io_util.h>
 #include <linux/wakelock.h>
-#include <linux/proc_fs.h>
 #include "gf_spi.h"
 
 #if defined(USE_SPI_BUS)
@@ -67,8 +66,6 @@
 #define	CHRD_DRIVER_NAME	"goodix_fp_spi"
 #define	CLASS_NAME		    "goodix_fp"
 
-#define PROC_NAME  "hwinfo"
-
 #define N_SPI_MINORS		32	/* ... up to 256 */
 static int SPIDEV_MAJOR;
 
@@ -77,7 +74,6 @@ static LIST_HEAD(device_list);
 static DEFINE_RT_MUTEX(device_list_lock);
 static struct wake_lock fp_wakelock;
 static struct gf_dev gf;
-static struct proc_dir_entry *proc_entry;
 
 #if 0
 static struct gf_key_map maps[] = {
@@ -812,14 +808,6 @@ static int gf_probe(struct platform_device *pdev)
 
 	wake_lock_init(&fp_wakelock, WAKE_LOCK_SUSPEND, "fp_wakelock");
 
-	proc_entry = proc_create(PROC_NAME, 0644, NULL, &proc_file_ops);
-	if (NULL == proc_entry) {
-		printk("gf3258 Couldn't create proc entry!");
-		return -ENOMEM;
-	} else {
-		printk("gf3258 Create proc entry success!");
-	}
-
 	pr_info("version V%d.%d.%02d\n", VER_MAJOR, VER_MINOR, PATCH_LEVEL);
 
 	return status;
@@ -867,7 +855,6 @@ static int gf_remove(struct platform_device *pdev)
 	list_del(&gf_dev->device_entry);
 	device_destroy(gf_class, gf_dev->devt);
 	clear_bit(MINOR(gf_dev->devt), minors);
-	remove_proc_entry(PROC_NAME,NULL);
 
 	rt_mutex_unlock(&device_list_lock);
 
