73ed9165c73a59b4ea6d5ca6ea91491c666c173e
Oktapra Amtono <oktapra.amtono@gmail.com>
2021-05-25 13:41:16 +0700

    treewide: silent log spam in dmesg
    Signed-off-by: Oktapra Amtono <oktapra.amtono@gmail.com>

--- drivers/mmc/host/sdhci-msm-ice.c
+++ drivers/mmc/host/sdhci-msm-ice.c
@@ -487,7 +487,7 @@ int sdhci_msm_ice_resume(struct sdhci_host *host)
 
 	if (msm_host->ice.state !=
 			SDHCI_MSM_ICE_STATE_SUSPENDED) {
-		pr_err("%s: ice is in invalid state before resume %d\n",
+		pr_debug("%s: ice is in invalid state before resume %d\n",
 			mmc_hostname(host->mmc), msm_host->ice.state);
 		return -EINVAL;
 	}
@@ -513,7 +513,7 @@ int sdhci_msm_ice_suspend(struct sdhci_host *host)
 
 	if (msm_host->ice.state !=
 			SDHCI_MSM_ICE_STATE_ACTIVE) {
-		pr_err("%s: ice is in invalid state before resume %d\n",
+		pr_debug("%s: ice is in invalid state before resume %d\n",
 			mmc_hostname(host->mmc), msm_host->ice.state);
 		return -EINVAL;
 	}
